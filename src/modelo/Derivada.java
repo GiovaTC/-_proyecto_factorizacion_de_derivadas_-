e720ada843609a16915fbdf651cdc958da0a26e6
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-18 15:02:24 -0500

    commit - Derivada / .

--- src/modelo/Derivada.java
+++ src/modelo/Derivada.java
@@ -1,4 +1,32 @@
 package modelo;
 
 public class Derivada {
+
+    private String funcion;
+    private String derivada;
+    private String factorizacion;
+
+    public String getFuncion() {
+        return funcion;
+    }
+
+    public void setFuncion(String funcion) {
+        this.funcion = funcion;
+    }
+
+    public String getDerivada() {
+        return derivada;
+    }
+
+    public void setDerivada(String derivada) {
+        this.derivada = derivada;
+    }
+
+    public String getFactorizacion() {
+        return factorizacion;
+    }
+
+    public void setFactorizacion(String factorizacion) {
+        this.factorizacion = factorizacion;
+    }   
 }
